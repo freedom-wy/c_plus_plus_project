ddd1d4583328ff202f5026fe913173aa8de7ecc5
dazhuang <dazhuang_python@sina.com>
2022-10-20 15:03:43 +0800

    初始化通讯录项目代码

---
+++ project/address_book/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#include "menu.h"
+
+
+int main()
+{
+    showMenu();
+    return 0;
+}
---
+++ project/address_book/menu.cpp
@@ -0,0 +1,13 @@
+#include "menu.h"
+
+// 菜单
+void showMenu()
+{
+    cout << "1、添加联系人" << endl;
+    cout << "2、显示联系人" << endl;
+    cout << "3、删除联系人" << endl;
+    cout << "4、查找联系人" << endl;
+    cout << "5、修改联系人" << endl;
+    cout << "6、清空联系人" << endl;
+    cout << "7、退出联系人" << endl;
+}
