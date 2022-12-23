483eeafacb9deb2318f137daf6c9bdb940c9dec7
dazhuang <dazhuang_python@sina.com>
2022-12-23 18:21:24 +0800

    C语言和进制转换

--- sample/c_code/base/hello_world/main.c
+++ sample/c_code/base/hello_world/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     printf("hello world\n");
+    system("ls");
     return 0;
 }
---
+++ sample/c_code/base/hello_world/main.s
@@ -0,0 +1,33 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 12, 3	sdk_version 12, 3
+	.globl	_main                           ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	leaq	L_.str(%rip), %rdi
+	movb	$0, %al
+	callq	_printf
+	leaq	L_.str.1(%rip), %rdi
+	callq	_system
+	xorl	%eax, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"hello world\n"
+
+L_.str.1:                               ## @.str.1
+	.asciz	"ls"
+
+.subsections_via_symbols
