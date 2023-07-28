566e423936ae52745c4d554ba006fd01ebd67eac
freedom-wy <dazhuang_python@sina.com>
2023-07-28 17:50:01 +0800

    多态

--- kr/多态/多态/main.cpp
+++ kr/多态/多态/main.cpp
@@ -84,16 +84,101 @@ void test2()
 	//SkillptrType spt = (SkillptrType)&CSodier::Skill; // 类成员指针赋值
 	//CSodier c1(1);
 	//(&c1->*spt)(1); // 类成员指针调用
-	agePtrType agePtr = &CSodier::age; // 类属性指针赋值
-	CSodier c2(1);
-	cout << "战士的年龄: " << (&c2->*agePtr) << endl;
+	//agePtrType agePtr = &CSodier::age; // 类属性指针赋值
+	//CSodier c2(1);
+	//cout << "战士的年龄: " << (&c2->*agePtr) << endl;
+	CSodier c3(0x11111111);
+	CHero& c4 = c3;
+	c4.Skill(1);
+	CHero* c2 = new CSodier(0x11111111);
+	c2->Skill(1);
+	cout << "hello world" << endl;
 }
 
+
+/* 虚函数（函数覆盖）原理
+对象首四个字节的地址：虚表指针
+虚函数地址的数组：虚表
+
+内存结构：
+CB b;
+|------------|        虚表(vftable)
+|__vfptr     | -----> |----------|
+|m_nA        |        |&CB:Foo   |
+|m_nB        |        |&CB::Foo1 |
+|------------|        |----------|
+
+编译期：
+   CB b;
+   1）拷贝父类的虚表{&CA:Foo, &CA::Foo1}
+   2）将子类重写了父类的虚函数地址替换虚表中的对应的地址{&CB:Foo, &CB::Foo1}
+
+运行期：
+   1.构造对象 new CB
+	 将自己的虚表的地址填入对象的虚表指针（__vfptr，位于对象首四个字节）
+   2.调用虚函数
+	 先从对象首四个字节获取虚表指针，然后通过虚表指针获取虚表，然后通过虚表获取虚函数的地址，调用
+*/
+
+class CA
+{
+public:
+	CA()
+	{
+		cout << "CA::CA" << endl;
+	}
+	virtual void Foo1() { cout << "CA::Foo1" << endl; }
+	virtual void Foo() { cout << "CA::Foo" << endl; }
+	int m_nA = 0xAAAAAAAA;
+};
+
+class CB :public CA
+{
+public:
+	CB()
+	{
+		cout << "CB::CB" << endl;
+	}
+	void Foo() { cout << "CB::Foo" << endl; }
+	void Foo1() { cout << "CB::Foo1" << endl; }
+	int m_nB = 0xBBBBBBBB;
+};
+
+class CD :public CA
+{
+public:
+	CD()
+	{
+		cout << "CD::CD" << endl;
+	}
+	virtual void Foo() { cout << "CD::Foo" << endl; }
+	virtual void Foo1() { cout << "CD::Foo1" << endl; }
+	void Test()
+	{
+		CA::Foo();
+	}
+	int m_nB = 0xDDDDDDDD;
+};
+
+void printFoo(CA& c)
+{
+	c.Foo();
+	cout << "hello world" << endl;
+}
+
+int test3()
+{
+	CB c1;
+	printFoo(c1);
+	CA* c2 = new CB();
+	c2->Foo();
+	return 0;
+}
 
 int main()
 {
 	//test1();
-	test2();
-	cout << "hello world" << endl;
+	//test2();
+	test3();
 	return 0;
 }
