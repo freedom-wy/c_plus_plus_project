e4b1ec453da31d78914a4c05e12af4eafd872561
freedom-wy <dazhuang_python@sina.com>
2023-08-04 17:50:55 +0800

    lambda表达式

---
+++ kr/lambda表达式/lambda表达式/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+typedef int(*FUNC)(int, float); // 函数指针
+
+int main()
+{
+	FUNC pfn = [](int n, float f)->int {
+		return n + f * 8;
+	};
+
+	int val = pfn(1, 1.1f);
+	cout << val << endl;
+	cout << "hello world" << endl;
+	return 0;
+}
