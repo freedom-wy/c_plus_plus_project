4dbecf9016ac9036711db43c09d5c0e12bba55b3
freedom-wy <dazhuang_python@sina.com>
2023-08-07 16:04:18 +0800

    右值引用和移动构造

--- kr/lambda表达式/lambda表达式/main.cpp
+++ kr/lambda表达式/lambda表达式/main.cpp
@@ -5,12 +5,19 @@ typedef int(*FUNC)(int, float); // 函数指针
 
 int main()
 {
-	FUNC pfn = [](int n, float f)->int {
+	/*FUNC pfn = [](int n, float f)->int {
 		return n + f * 8;
 	};
 
 	int val = pfn(1, 1.1f);
-	cout << val << endl;
+	cout << val << endl;*/
+
+	int a = 1;
+	int c = 2;
+	int& b = a;
+	b = 3;
+	//int& b = c; // 不能更改
+	cout << a << ", " << b << endl;
 	cout << "hello world" << endl;
 	return 0;
 }
---
+++ kr/新特性/新特性/main.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+
+void test1()
+{
+	// 右值引用
+	int a = 1;
+	int& b = a; // 左值引用
+	//int& c = 1; // 报错，不能对右值进行引用
+	int&& c = 1; // 右值引用
+	const int& d = 1; // 万能引用
+	cout << "a: " << a << ", c: " << c << endl;
+	cout << "hello world" << endl;
+}
+
+class CA
+{
+private:
+	int* mp;
+public:
+	CA(int n) :mp(new int(n)) { cout << "CA::CA(int)" << endl; }
+	~CA()
+	{
+		if (mp != NULL)
+		{
+			delete mp;
+		}
+		cout << "CA::~CA" << endl;
+	}
+	CA(const CA& obj)
+	{
+		cout << "拷贝构造" << endl;
+		this->mp = new int(*obj.mp);
+	}
+
+	CA(CA&& obj) // 当有移动构造时，不走拷贝构造
+	{
+		this->mp = obj.mp;
+		obj.mp = NULL;
+	}
+};
+
+CA foo()
+{
+	 // 创建一个函数
+	CA a(1);
+	return a; // 拷贝构造
+}
+
+void test2()
+{
+	CA b = foo();
+	cout << "hello world" << endl;
+}
+
+
+int main()
+{
+	//test1();
+	test2();
+	return 0;
+}
