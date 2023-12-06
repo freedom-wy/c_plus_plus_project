27fa5dc177348ba9c4285ae6f77d7a7b01319b31
freedom-wy <dazhuang_python@sina.com>
2023-12-06 17:00:59 +0800

    未解决异或为0的问题

--- jayx/异或加解密/异或加解密/main.c
+++ jayx/异或加解密/异或加解密/main.c
@@ -5,12 +5,13 @@
 
 
 // 对字符串加解密
-char str1[] = "hello world";
+char str1[] = "abcd1234";
 
 char* Encrypt(char* str, char* secretKey)
 {
+	int str_len = strlen(str);
 	// 加密
-	for (size_t i = 0; i < strlen(str); i++)
+	for (size_t i = 0; i < str_len; i++)
 	{
 		str[i] = str[i] ^ secretKey[i % strlen(secretKey)];
 	}
@@ -19,15 +20,62 @@ char* Encrypt(char* str, char* secretKey)
 
 char* Decrypt(char* str, char* secretKey)
 {
+	int str_len = strlen(str);
 	// 解密
-	for (size_t i = 0; i < strlen(str); i++)
+	for (size_t i = 0; i < str_len; i++)
 	{
 		str[i] = str[i] ^ secretKey[i % strlen(secretKey)];
 	}
 	return str;
 }
 
-// 对文件加解密
+void test2()
+{
+	char secretKey[] = "abc";
+	char* str2 = Encrypt(str1, secretKey);
+	printf("%s\n", str2);
+	char* str3 = Decrypt(str2, secretKey);
+	printf("%s\n", str3);
+}
+
+
+// 对文件内容加解密
+
+//// 获取文件大小
+//int fsize(FILE* fp)
+//{
+//	fpos_t fpos; // 保存指针位置
+//	fgetpos(fp, &fpos); // 获取当前指针位置
+//	fseek(fp, 0, SEEK_END); // 移动指针到文件末尾
+//	int n = ftell(fp); // 获取文件大小
+//	fsetpos(fp, &fpos); // 恢复位置
+//	return n;
+//}
+void EncryptAndDecryptFile(char* readFilePath, char* writeFilePath, char* secretKey)
+{
+	FILE* f_read = fopen(readFilePath, "r");
+	if (f_read == NULL)
+	{
+		printf("文件打开失败\n");
+		return;
+	}
+	// 按行读取文件内容
+	while (!feof(f_read))
+	{
+		char buf[1024] = { '0' };
+		char* p = fgets(buf, sizeof(buf)-1, f_read);
+		if (p == NULL)
+		{
+			break;
+		}
+		buf[strlen(buf)] = '\0';
+		// 加密数据
+		char* encrypt_str = Encrypt(buf, secretKey);
+		char* decrypt_str = Decrypt(encrypt_str, secretKey);
+		printf("行内容: %s\n", decrypt_str);
+	}
+	fclose(f_read);
+}
 
 
 
@@ -44,67 +92,17 @@ void test1()
 	printf("0X%x\n", c);
 }
 
-void test2()
-{
-	char secretKey[] = "abc";
-	char* str2 = Encrypt(str1, secretKey);
-	printf("%s\n", str2);
-	char* str3 = Decrypt(str2, secretKey);
-	printf("%s\n", str3);
-}
+
 
 void test3()
 {
-	FILE* f_read = fopen("source.txt", "r");
-	FILE* f_write = fopen("dest.txt", "a+");
-	if (f_read == NULL)
-	{
-		printf("f_read文件打开失败\n");
-		return 0;
-	}
-	if (f_write == NULL)
-	{
-		printf("f_write文件打开失败\n");
-		return 0;
-	}
-	char c = 'a';
-	while ((c = fgetc(f_read)) != EOF)
-	{
-		c = c ^ 'a';
-		fputc(c, f_write);
-	}
-	fclose(f_read);
-	fclose(f_write);
+	EncryptAndDecryptFile("test.txt", "abc", "abc");
 }
 
-void test4()
-{
-	FILE* f_read = fopen("dest.txt", "r");
-	FILE* f_write = fopen("source1.txt", "a+");
-	if (f_read == NULL)
-	{
-		printf("f_read文件打开失败\n");
-		return 0;
-	}
-	if (f_write == NULL)
-	{
-		printf("f_write文件打开失败\n");
-		return 0;
-	}
-	char c = 'a';
-	while ((c = fgetc(f_read)) != EOF)
-	{
-		c = c ^ 'a';
-		fputc(c, f_write);
-	}
-	fclose(f_read);
-	fclose(f_write);
-}
+
 
 int main()
 {
-	//test2();
-	//test3();
-	// test4();
+	test2();
 	return 0;
 }
