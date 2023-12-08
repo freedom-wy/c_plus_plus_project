6db6e2310031994d701a78bb9b2786bf5c93d0df
freedom-wy <dazhuang_python@sina.com>
2023-12-08 16:14:23 +0800

    函数调用约定和平栈

--- jayx/函数调用约定/函数调用约定/main.cpp
+++ jayx/函数调用约定/函数调用约定/main.cpp
@@ -17,23 +17,26 @@ using namespace std;
 
 // __cdecl 是C/C++的默认调用约定，入栈顺序从右到左
 // __stdcall 是windows api默认调用约定，微软的WINAPI都是这个调用约定, 入栈顺序也是从右到左
+// __fastcall 不需要平栈，入栈顺序由右向左，最后两个参数放入ecx和edx,其他参数放入栈
 
 int add_func(int a, int b);
 
 int add_func(int a, int b)
 {
+	// c++默认函数调用方式__cdecl，由调用方平栈，即main方法平栈
 	return a + b;
 }
 
-//void test()
-//{
-//	cout << "test" << endl;
-//}
+void test()
+{
+	// c++默认函数调用方式__cdecl，由调用方平栈，由于没有参数，由return平栈
+	cout << "test" << endl;
+}
 
 int _tmain()
 {
-	//test();
+	// test();
 	add_func(1, 2);
-	//MessageBox(NULL, _T("消息框的标题"), _T("消息框的内容"), MB_YESNO);
+	// MessageBox(NULL, _T("消息框的标题"), _T("消息框的内容"), MB_YESNO);  // __stdcall由被调方法自己栈平衡
 	return 0;
 }
